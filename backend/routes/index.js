90ebf37a3839863d7b32a083445bdb0aa271ac07
abhishek <starsabhishek@gmail.com>
2022-04-28 11:36:19 -0500

    Phase 1 Completed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { asyncHandler, csrfProtection } = require("./utils");
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get('/hello/world', csrfProtection, function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
