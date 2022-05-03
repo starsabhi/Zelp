88c2e9e3c1367dbee347cebf6db8383a6512376f
abhishek <starsabhishek@gmail.com>
2022-05-02 19:12:48 -0500

    added one seed for Business

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -49,6 +49,7 @@ module.exports = (sequelize, DataTypes) => {
 
   User.associate = function(models) {
     // associations can be defined here
+    User.hasMany(models.Business, {foreignKey:"userId"})
 
   };
 
