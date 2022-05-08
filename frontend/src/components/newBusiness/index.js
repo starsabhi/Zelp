2c43ce7c9426153867628896af845627369687bc
abhishek <starsabhishek@gmail.com>
2022-05-07 19:54:51 -0500

    CSS COMPLETED FOR CREATE PAGE WITH CANCEL BUT

--- frontend/src/components/newBusiness/index.js
+++ frontend/src/components/newBusiness/index.js
@@ -2,7 +2,6 @@ import {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux"
 import { writeBusiness } from "../../store/business";
 import {Redirect,useHistory} from "react-router-dom"
-import logo from "../../images/logo.png"
 import "./NewBusiness.css"
 import navLogoZelp from '../../images/navLogoZelp.png'
 
@@ -24,6 +23,10 @@ const NewBusiness = () => {
 
   useEffect(()=>{
     let errors = [];
+    const phoneError = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/i ;
+    const stateError = /^(?:(A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|P[AR]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY]))$$/i;
+    const zipCodeError = /^[0-9]{5}(?:-[0-9]{4})?$/i
+
     if(!name.length) errors.push("Please Enter Valid Name")
     if(!category.length) errors.push("Please Enter Valid category")
     if(!description.length) errors.push("Please Enter Valid description")
@@ -33,6 +36,9 @@ const NewBusiness = () => {
     if(!zip_code.length) errors.push("Please Enter Valid zip code")
     if(!phone_number.length) errors.push("Please Enter Valid phone number")
     if(!image.length) errors.push("Please Enter Valid image")
+    if(!phoneError.test(phone_number)) errors.push("Phone Number should be 10 Digit")
+    if(!zipCodeError.test(zip_code)) errors.push("Please Enter Valid Zip Code e.g.75080")
+    if(!stateError.test(state)) errors.push("Please Enter Valid state")
     setValidationErrors(errors)
   },[name,category,description,address,city,state,zip_code,phone_number,image])
 
@@ -70,8 +76,16 @@ const NewBusiness = () => {
       history.push("/")
     }
   }
+
+  const handleCancel = (e) => {
+    e.preventDefault();
+    history.push("/")
+  }
+
+
+
   if(!sessionUser){
-    console.log("HEllo without user**************")
+    // console.log("HEllo without user**************")
     return (
       <>
     <Redirect to="/signup"></Redirect>
@@ -80,92 +94,109 @@ const NewBusiness = () => {
   }
 
   else{
-    console.log("HEllo wokring*********")
+    // console.log("HEllo wokring*********")
 
   return (
       <div className="inputBox">
-        <h1>Create Business</h1>
+        {/* <h1>Create Business</h1> */}
         <form onSubmit={handleSubmit}>
         <img className='navbarLogo' src={navLogoZelp}></img>
           <h2>Form to Create your Own Busniess</h2>
         {(setValidationErrors.length && hasSubmitted) ?
           <div id="ErrorDiv">
-            <ul>
+            <ul id="errorsForCreateUL">
               {validationErrors.map(error => (
-                <li key={error}>{error}</li>
+                <li id="ErrorsCreateBusinessform" key={error}>{error}</li>
               ))}
             </ul>
           </div>
           :<></>
         }
-          <label>
-          Name :
-          <input
+          <label>Name of Your Restaurant</label>
+          <input className="allInputforCreateB"
           type='text'
           onChange={(e)=>setName(e.target.value)}
           value={name}
           placeholder="Name"
           name="name"
           ></input>
-          </label>
 
-          <input
+          <label>Category</label>
+          <input className="allInputforCreateB"
           type='text'
           onChange={(e)=>setCategory(e.target.value)}
           value={category}
           placeholder="Category"
           name="category"
           ></input>
-          <textarea
-          type='text'
+
+          <label>Description</label>
+          <textarea className="textAreaforCreateB"
+          type='description'
           onChange={(e)=>setDescription(e.target.value)}
           value={description}
           placeholder="Description"
           name="description"
           ></textarea>
-          <input
+
+          <label>Address</label>
+          <input className="allInputforCreateB"
           type='text'
           onChange={(e)=>setAddress(e.target.value)}
           value={address}
           placeholder="Address"
           name="address"
           ></input>
-          <input
+
+          <label>City</label>
+          <input className="allInputforCreateB"
           type='text'
           onChange={(e)=>setCity(e.target.value)}
           value={city}
           placeholder="City"
           name="city"
           ></input>
-          <input
+
+
+          <label>State</label>
+          <input className="allInputforCreateB"
           type='text'
           onChange={(e)=>setState(e.target.value)}
           value={state}
-          placeholder="State"
+          placeholder="e.g.TX/AZ"
           name="state"
           ></input>
-          <input
+
+          <label>Zip Code</label>
+          <input className="allInputforCreateB"
           type='text'
           onChange={(e)=>setZipcode(e.target.value)}
           value={zip_code}
           placeholder="Zip Code"
           name="zip_code"
           ></input>
-          <input
+
+          <label>Phone Number</label>
+          <input className="allInputforCreateB"
           type='text'
           onChange={(e)=>setPhone_Number(e.target.value)}
           value={phone_number}
-          placeholder="Phone Number"
+          placeholder="phone number"
           name="Phone Number"
           ></input>
-          <input
+
+          <label>Image URL</label>
+          <input className="allInputforCreateB"
           type='text'
           onChange={(e)=>setImage(e.target.value)}
           value={image}
-          placeholder="Image"
+          placeholder="Image URL"
           name="image"
           ></input>
-          <button>Submit</button>
+          <div className="subNcanBtn">
+          <button className="createBsumbitBtn" id="createBsumbitBtnID">Submit</button>
+          <button className="createBsumbitBtn" onClick={handleCancel}>Cancel</button>
+          </div>
         </form>
       </div>
   );
