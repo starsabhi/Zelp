a226b08c85abc11acf7001d0263164a93ffbe46e
abhishek <starsabhishek@gmail.com>
2022-07-13 16:32:03 -0500

    Splash PAGE CSS

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,9 +1,28 @@
+import { NavLink } from 'react-router-dom';
 import Footer from '../Footer';
 import './SplashPage.css';
 function SplashPage() {
   return (
     <>
-      <div className="splashPage">Hello</div>
+      <div className="splashPage">
+        <div className="mainDivForSP">
+          <div className="leftSideofSplashPAge">
+            <div className="splashPageinnerText">
+              <div className="welcomeDiv">Welcome to Zelp!</div>
+              <span className="smallinfoForSLPASH">
+                Join our network by adding your business!!
+              </span>
+              <div className="innerZelpBUTN">
+                <NavLink to="/">
+                  <button className="innerZelpBUTNREAL">Our Restaurants</button>
+                </NavLink>
+              </div>
+            </div>
+            {/* <div className="innerZelpBUTNREAL1255">Our Restaurants</div> */}
+          </div>
+          <div className="rightSideofSplashPAge"></div>
+        </div>
+      </div>
       <Footer />
     </>
   );
